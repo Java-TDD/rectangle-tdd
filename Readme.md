255080f7e619e14720b11c02a75ce35a184efa4c
YugendarM <yugendar2411@gmail.com>
2024-07-30 09:56:52 +0530

    Update - created suit for Area of Rectangle

--- Readme.md
+++ Readme.md
@@ -9,5 +9,6 @@ As a fan of geometry, I want to find the area of the rectangle.
 #### TestCase: 
 - The method `calculateArea()` is called with the created instance and the area of the rectangle is obtained by the method.
 - `assertThat()` method is used to evaluate the equality of the expected area and the actual area.
+- Created a suit with `@Nested` annotation to suit up the testcases for calculating the area of the rectangle.
 
 
--- src/test/java/com/example/RectangleTest.java
+++ src/test/java/com/example/RectangleTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -8,21 +9,24 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class RectangleTest {
 
-    @Test
-    public void toReturnEightAsAreaOfRectangleWhenLengthAndBreadthIsFourAndTwo(){
-        int four = 4, two = 2, eight = 8;
-        Rectangle rectangle = new Rectangle(four, two);
+    @Nested
+    class Area {
+        @Test
+        public void toReturnEightAsAreaOfRectangleWhenLengthAndBreadthIsFourAndTwo(){
+            int four = 4, two = 2, eight = 8;
+            Rectangle rectangle = new Rectangle(four, two);
 
-        int areaOfRectangle4x2 = rectangle.calculateArea();
-        assertThat(areaOfRectangle4x2, is(equalTo(eight)));
-    }
+            int areaOfRectangle4x2 = rectangle.calculateArea();
+            assertThat(areaOfRectangle4x2, is(equalTo(eight)));
+        }
 
-    @Test
-    public void toReturnZeroAsAreaOfRectangleWhenLengthAndBreadthIsZeroAndThree(){
-        int zero = 0, three = 3;
-        Rectangle rectangle = new Rectangle(zero, three);
+        @Test
+        public void toReturnZeroAsAreaOfRectangleWhenLengthAndBreadthIsZeroAndThree(){
+            int zero = 0, three = 3;
+            Rectangle rectangle = new Rectangle(zero, three);
 
-        int areaOfRectangle0x3 = rectangle.calculateArea();
-        assertThat(areaOfRectangle0x3, is(equalTo(zero)));
+            int areaOfRectangle0x3 = rectangle.calculateArea();
+            assertThat(areaOfRectangle0x3, is(equalTo(zero)));
+        }
     }
 }
